f81968c1a119b1a257fc072264ed5f26403e6ccc
Jimmy Joy <jimmypjoy@gmail.com>
2018-07-14 16:58:39 -0400

    adding demo java project for spring

---
+++ spring-demo/src/main/java/com/example/springdemo1/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springdemo1;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/greetings")
+    public String index() {
+        System.out.println("inside test program");
+    	return "Greetings from Spring Boot jimmy!";
+    }
+
+}
