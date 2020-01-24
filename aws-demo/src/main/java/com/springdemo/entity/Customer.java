fe169ab68a3f37e5c18929d0b05d0c2d3111cc7e
Jimmy Joy <35603896+jimmypjoy@users.noreply.github.com>
2020-01-24 16:40:55 -0500

    adding aws-demo to git

---
+++ aws-demo/src/main/java/com/springdemo/entity/Customer.java
@@ -0,0 +1,113 @@
+package com.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "address1")
+	private String address1;
+
+	@Column(name = "city")
+	private String city;
+
+	@Column(name = "state")
+	private String state;
+
+	@Column(name = "country")
+	private String country;
+
+	public Customer() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ aws-demo/src/main/webapp/WEB-INF/view/customer-form.jsp
@@ -0,0 +1,103 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Save Customer</title>
+
+	<link type="text/css"
+		  rel="stylesheet"
+		  href="${pageContext.request.contextPath}/resources/css/style.css">
+
+	<link type="text/css"
+		  rel="stylesheet"
+		  href="${pageContext.request.contextPath}/resources/css/add-customer-style.css">
+</head>
+
+<body>
+	
+	<div id="wrapper">
+		<div id="header">
+			<h2>Sample App</h2>
+		</div>
+	</div>
+
+	<div id="container">
+		<h3>Save Customer</h3>
+	
+		<form:form action="saveCustomer" modelAttribute="customer" method="POST">
+
+			<!-- need to associate this data with customer id -->
+			<form:hidden path="id" />
+					
+			<table>
+				<tbody>
+					<tr>
+						<td><label>First name:</label></td>
+						<td><form:input path="firstName" /></td>
+					</tr>
+				
+					<tr>
+						<td><label>Last name:</label></td>
+						<td><form:input path="lastName" /></td>
+					</tr>
+
+					<tr>
+						<td><label>Email:</label></td>
+						<td><form:input path="email" /></td>
+					</tr>
+					
+					<tr>
+						<td><label>Address:</label></td>
+						<td><form:input path="address1" /></td>
+					</tr>
+					
+					<tr>
+						<td><label>City:</label></td>
+						<td><form:input path="city" /></td>
+					</tr>
+					
+					<tr>
+						<td><label>State:</label></td>
+						<td><form:input path="state" /></td>
+					</tr>
+					
+					<tr>
+						<td><label>Country:</label></td>
+						<td><form:input path="country" /></td>
+					</tr>
+
+					<tr>
+						<td><label></label></td>
+						<td><input type="submit" value="Save" class="save" /></td>
+					</tr>
+
+				
+				</tbody>
+			</table>
+		
+		
+		</form:form>
+	
+		<div style="clear; both;"></div>
+		
+		<p>
+			<a href="${pageContext.request.contextPath}/customer/list">Back to List</a>
+		</p>
+	
+	</div>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
